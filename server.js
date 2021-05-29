a7429fe574802bf0f5512ed3cda0cc6b9e51c320
sanjay rajagopalan new <srajagop10@Tejas-MacBook.local>
2021-05-29 12:26:57 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require ("express")
+const app = express()
+const bodyParser = require ("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+var apiKey = 'cd41f9199d1fafe6b40d1d2f4647b00a';
+
+app.set('view engine','ejs')
+
+app.get('/',function(req,res){
+    //body
+    res.render('index')
+})
+
+app.post('/',function(req,res){
+    //body
+    console.log(req.body.city)
+    var city = req.body.city;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    request(url,function(err,response,body){
+        if(err){
+            res.render('index',{weather:null,error:'Error: Please Try Again'})
+        }
+        else{
+            var weather = JSON.parse(body)
+            if(weather.main==undefined){
+                res.render('index',{weather:null,error:'Error: Please Try Again'})
+            }
+            else{
+                var weathertext = `it is ${weather.main.temp} degrees in ${weather.name}`
+                res.render('index',{weather:text,error:null})
+            }
+        }
+    })
+})
+
+app.listen(3000,function(){
+    console.log('app running on 3000')
+})
